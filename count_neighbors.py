f38c2918fca214a6860b7a575d8b48d08964db9f
mouryachimpiri <mouryachimpiri@gmail.com>
2023-10-19 20:50:42 -0400

    Add function comments and a proper docstring

--- count_neighbors.py
+++ count_neighbors.py
@@ -1,15 +1,24 @@
 import numpy as np
 
 
-def countneighbors(A, row, col):
+def countneighbors(A, row, col):  # Finds the number of True neighbors around an element in matrix A
+    countneighbors.__doc__ = ("Finds the number of true neighbors to evolve a matrix in Conway's Game of Life \n"
+                              "Inputs- A: MxN boolean matrix \n"
+                              "\t row: row number of element within A \n"
+                              "\t col: column number of element within A \n"
+                              "Output- count: number of true neighbors in 8 surrounding elements")
     count = 0
     A_rows, A_cols = A.shape
+
+    # Finds the number of true elements in the 8 surrounding elements
     for i in [-1, 0, 1]:
         for j in [-1, 0, 1]:
-            if i == 0 and j == 0:
+            if i == 0 and j == 0:  # Ignores the element in consideration
                 continue
+            # Wraps the edges of the matrix together
             neighbor_row = (row + i) % A_rows
             neighbor_col = (col + j) % A_cols
+
             if A[neighbor_row, neighbor_col]:
                 count += 1
     return count
--- evolve.py
+++ evolve.py
@@ -2,16 +2,21 @@ import numpy as np
 import count_neighbors
 
 
-def evolve(A):
+def evolve(A):  # Details the rules for Conway's Game of Life
+    evolve.__doc__ = ("Details how the matrix evolves in Conway's Game of Life \n"
+                      "Input- A: MxN boolean matrix \n"
+                      "Output- A: MxN updated matrix")
     rows, cols = A.shape
-    evolved_A = np.copy(A)
+    evolved_A = np.copy(A)  # Creates a copy of A to edit
     for i in range(rows):
         for j in range(cols):
             if not A[i, j]:
-                if count_neighbors.countneighbors(A, i, j) == 3:
+                if count_neighbors.countneighbors(A, i, j) == 3:  # If a false element has 3 True neighbors,
+                    # it becomes True
                     evolved_A[i, j] = True
             else:
-                if count_neighbors.countneighbors(A, i, j) > 3 or count_neighbors.countneighbors(A, i, j) < 2:
+                if count_neighbors.countneighbors(A, i, j) > 3 or count_neighbors.countneighbors(A, i, j) < 2:  # If a
+                    # true element has either more than 3 True neighbors or less than 2 True neighbors, it becomes false
                     evolved_A[i, j] = False
 
     A = evolved_A
